54e20ad2c469a9e5875810f77fda0fdcdf91ad7c
dhanush <dhanush.prabhu@outlook.com>
2024-10-18 21:47:43 +0530

    addded directions button and marker to map

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,13 @@ import Map from "../map-dir/map";
 import { FaFacebook } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { RiYoutubeLine } from "react-icons/ri";
+
 const Footer = () => {
   return (
     <>
       <footer className="text-white bg-black md:text-xl text-sm h-auto">
         <div className="md:flex flex-row justify-evenly text-white bg-black md:text-xl text-sm h-auto">
+          {/* Contact Info and Social Links Section */}
           <div className="flex flex-col justify-evenly md:space-y-4 space-y-6 ">
             <div className="flex flex-row md:justify-evenly w-[100%] space-x-28 mt-20 justify-center">
               <div className="flex flex-col space-y-2">
@@ -41,16 +43,29 @@ const Footer = () => {
               </div>
             </div>
           </div>
-          <div className="flex justify-center mb-4 mt-8 lg:mt-28   ">
-            <div className="items-center mt-6 w-full h-full lg:w-[500px] lg:h-[500px] overflow-hidden rounded-xl z-0 px-1">
+
+          {/* Map and Direction Button Section */}
+          <div className="flex flex-col items-center mb-4 mt-8 lg:mt-28">
+            <button
+              className="bg-gradient-to-r from-blue-500 to-teal-400 text-white font-semibold py-3 px-6 rounded-full shadow-lg hover:from-teal-400 hover:to-blue-500 hover:shadow-xl transition duration-300 ease-in-out mb-6 lg:mb-8"
+              onClick={() => {
+                const destinationLatitude = 13.344513;
+                const destinationLongitude = 77.181013;
+                const googleMapsUrl = `https://www.google.com/maps/dir/?api=1&destination=${destinationLatitude},${destinationLongitude}`;
+                window.open(googleMapsUrl, "_blank");
+              }}
+            >
+              Get Directions to Our Resort
+            </button>
+            <div className="w-full h-full lg:w-[500px] lg:h-[500px] overflow-hidden rounded-xl z-0 px-1">
               <Map />
             </div>
           </div>
         </div>
-        {/* <br />
-        <br />*/}
+
+        {/* Footer Bottom Section */}
         <br />
-        <div className="  text-center">
+        <div className="text-center">
           Copyright &copy;2024 ; Developed by Team
         </div>
         <br />
@@ -58,4 +73,5 @@ const Footer = () => {
     </>
   );
 };
+
 export default Footer;
--- src/map-dir/map.js
+++ src/map-dir/map.js
@@ -8,17 +8,17 @@ const Map = () => {
     lng: 77.184534,
   };
 
-  // Define container style using Tailwind
+  // Define container style for the map
   const containerStyle = {
     width: "100%",
     height: "500px", // Adjust this for your layout
   };
 
-  // Custom icon for the marker
-  // const customMarkerIcon = {
-  //   url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dreamstime.com%2Fillustration%2Fgoogle-map-marker.html&psig=AOvVaw0fu3ntVKx0lKOtav_kVaP9&ust=1729351505157000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIj1iseemIkDFQAAAAAdAAAAABAE", // Replace with the URL of your icon image
-  //   scaledSize: { width: 50, height: 50 }, // Adjust size as needed
-  // };
+  // Custom black marker icon using the provided SVG
+  const customMarkerIcon = {
+    url: "data:image/svg+xml;charset=UTF-8,%3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns:sketch='http://www.bohemiancoding.com/sketch/ns' viewBox='0 0 30 37.5' version='1.1' x='0px' y='0px'%3E%3Ctitle%3Eaction_077-location-map-marker-pin-place%3C/title%3E%3Cdesc%3ECreated with Sketch.%3C/desc%3E%3Cg stroke='none' stroke-width='1' fill='none' fill-rule='evenodd' sketch:type='MSPage'%3E%3Cg sketch:type='MSArtboardGroup' transform='translate(-315.000000, -360.000000)' fill='%23000000'%3E%3Cpath d='M330.2468,386.332577 C330.521957,385.957769 330.828389,385.532919 331.158301,385.066171 C332.100875,383.732652 333.043481,382.329678 333.923914,380.92199 C334.490728,380.015735 335.013548,379.135938 335.483281,378.291917 C337.092557,375.400353 338,373.08905 338,371.5 C338,366.80558 334.19442,363 329.5,363 C324.80558,363 321,366.80558 321,371.5 C321,373.08905 321.907443,375.400353 323.516719,378.291917 C323.986452,379.135938 324.509272,380.015735 325.076086,380.92199 C325.956519,382.329678 326.899125,383.732652 327.841699,385.066171 C328.171611,385.532919 328.478043,385.957769 328.7532,386.332577 C328.918693,386.558005 329.037279,386.717043 329.101156,386.801535 C329.301214,387.066155 329.698786,387.066155 329.898844,386.801535 C329.962721,386.717043 330.081307,386.558005 330.2468,386.332577 L330.2468,386.332577 Z M329.5,374 C331.432997,374 333,372.432997 333,370.5 C333,368.567003 331.432997,367 329.5,367 C327.567003,367 326,368.567003 326,370.5 C326,372.432997 327.567003,374 329.5,374 L329.5,374 Z' sketch:type='MSShapeGroup'/%3E%3C/g%3E%3C/g%3E%3Ctext x='0' y='45' fill='%23000000' font-size='5px' font-weight='bold' font-family='Helvetica Neue, Helvetica, Arial-Unicode, Arial, Sans-serif'%3ECreated by Pham Thi Dieu Linh%3C/text%3E%3Ctext x='0' y='50' fill='%23000000' font-size='5px' font-weight='bold' font-family='Helvetica Neue, Helvetica, Arial-Unicode, Arial, Sans-serif'%3Efrom the Noun Project%3C/text%3E%3C/svg%3E",
+    scaledSize: { width: 50, height: 50 }, // Adjust size as needed
+  };
 
   return (
     <div className="flex justify-center items-center">
@@ -28,8 +28,8 @@ const Map = () => {
           center={centerLocation}
           zoom={13} // Adjust the zoom level as needed
         >
-          {/* Marker with custom icon and label */}
-          <Marker position={centerLocation} />
+          {/* Marker with the custom black SVG icon */}
+          <Marker position={centerLocation} icon={customMarkerIcon} />
         </GoogleMap>
       </LoadScript>
     </div>
