4aef132e505946c5926f7d77216a41ce074180b7
dhanush <dhanush.prabhu@outlook.com>
2024-10-18 20:58:43 +0530

    marker set

--- src/map-dir/map.js
+++ src/map-dir/map.js
@@ -4,21 +4,21 @@ import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 const Map = () => {
   // Location to be centered on and where the markers will be placed
   const centerLocation = {
-    lat: 13.346434,
+    lat: 13.346434, // Adjusted to the latest location
     lng: 77.184534,
   };
-
-  // Second marker location
-  // const secondLocation = {
-  //   lat: 13.348434, // Slightly different latitude
-  //   lng: 77.182534, // Slightly different longitude
-  // };
 
   // Define container style using Tailwind
   const containerStyle = {
     width: "100%",
     height: "500px", // Adjust this for your layout
   };
+
+  // Custom icon for the marker
+  // const customMarkerIcon = {
+  //   url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dreamstime.com%2Fillustration%2Fgoogle-map-marker.html&psig=AOvVaw0fu3ntVKx0lKOtav_kVaP9&ust=1729351505157000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIj1iseemIkDFQAAAAAdAAAAABAE", // Replace with the URL of your icon image
+  //   scaledSize: { width: 50, height: 50 }, // Adjust size as needed
+  // };
 
   return (
     <div className="flex justify-center items-center">
@@ -28,8 +28,16 @@ const Map = () => {
           center={centerLocation}
           zoom={13} // Adjust the zoom level as needed
         >
-          {/* Marker at the center location */}
-          <Marker position={centerLocation} />
+          {/* Marker with custom icon and label */}
+          <Marker
+            position={centerLocation}
+            label={{
+              text: "My Resort", // Label text
+              color: "black", // Label color
+              fontWeight: "bold", // Make it bold
+              fontSize: "16px", // Adjust size as needed
+            }}
+          />
         </GoogleMap>
       </LoadScript>
     </div>
