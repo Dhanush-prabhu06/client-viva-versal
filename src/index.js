eca40af6afea52df02f1bd525c8adb539e0910e5
dhanush <dhanush.prabhu@outlook.com>
2024-11-11 20:27:48 +0530

    analytics added

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 //import Facilities from "./Screens/landing-compo/Facilities";
 import WA_E from "../src/components/WA_E";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <WA_E />
     <Header />
+    <Analytics />
     <App />
     <Footer />
   </React.StrictMode>
